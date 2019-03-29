a15e253145b12232fd37ead10d307fc5784864ef
Christian-Martinez <christ96mart@gmail.com>
2019-03-29 02:18:17 -0400

    Recycler view

---
+++ app/src/main/java/com/example/top/Artista.java
@@ -0,0 +1,126 @@
+package com.example.top;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.Objects;
+
+public class Artista {
+    public static final String ORDEN = "orden";
+
+    private long id;
+    private String nombre;
+    private String apellidos;
+    private long fechaNacimiento;
+    private String lugarNacimiento;
+    private short estatura;
+    private String notas;
+    private int orden;
+    private String fotoUrl;
+
+    public Artista() {
+    }
+
+    public Artista(long id, String nombre, String apellidos, long fechaNacimiento,
+                   String lugarNacimiento, short estatura, String notas, int orden, String fotoUrl) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.lugarNacimiento = lugarNacimiento;
+        this.estatura = estatura;
+        this.notas = notas;
+        this.orden = orden;
+        this.fotoUrl = fotoUrl;
+    }
+
+    public static String getORDEN() {
+        return ORDEN;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public long getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(long fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getLugarNacimiento() {
+        return lugarNacimiento;
+    }
+
+    public void setLugarNacimiento(String lugarNacimiento) {
+        this.lugarNacimiento = lugarNacimiento;
+    }
+
+    public short getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(short estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Artista)) return false;
+        Artista artista = (Artista) o;
+        return id == artista.id;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

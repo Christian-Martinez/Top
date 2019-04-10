006aeda7c51bfa53e6c6c238b026d600e8995c99
Christian-Martinez <christ96mart@gmail.com>
2019-04-09 23:49:07 -0400

    import a androidx, edit declaracEinstanc de reciclerView, usa/edit extends, import, recursos XML y gradle

--- app/src/main/java/com/example/top/MainActivity.java
+++ app/src/main/java/com/example/top/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.top;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.ArrayList;
 
